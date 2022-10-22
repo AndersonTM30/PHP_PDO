93b1e7988235c6e431b067d748c368f91f79028b
AndersonTM30 <andersondev30@gmail.com>
2022-10-22 12:31:44 -0300

    Alterando a listagem para retornar como objeto

--- lista-alunos.php
+++ lista-alunos.php
@@ -1,5 +1,7 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $pdo = new PDO('sqlsrv:Server=localhost,1433;Database=Teste', 'anderson', '1234');
@@ -7,6 +9,15 @@ $pdo = new PDO('sqlsrv:Server=localhost,1433;Database=Teste', 'anderson', '1234'
 // Query para consultar os alunos
 $statement = $pdo->query('SELECT * FROM students;');
 // Retorna a lista de alunos
-$studentList = $statement->fetchAll();
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+
+foreach($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+}
 // imprime na tela um array associativo
-echo $studentList[0]['name'];
+// echo $studentList[0]['name'];
+var_dump($studentList);
