640dfd437b2e24193d0b2b2cbc65962582aead91
AndersonTM30 <andersondev30@gmail.com>
2022-10-25 21:37:09 -0300

    Definindo os parâmetros nomeados na query de inserção de aluno

--- inserir-aluno.php
+++ inserir-aluno.php
@@ -8,13 +8,13 @@ require_once 'vendor/autoload.php';
 // Cria uma nova conexão (não é recomendado fazer desta forma, feito apenas para fins de estudo)
 $pdo = new PDO('sqlsrv:Server=localhost,1433;Database=Teste', 'anderson', '1234');
 // instancia um novo objeto de estudante para passar os valores
-$student = new Student("Anderson', ''); DROP TABLE students; -- Ferreira", new \DateTimeImmutable('1992-06-03'));
+$student = new Student("Alisson Sandes", new \DateTimeImmutable('1995-06-03'));
 
 // query de inserção no banco de dados
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('U-m-d'));
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 if ($statement->execute()) {
     echo "Aluno incluído";
 }
