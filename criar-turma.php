6b2a87c4d5c0e893334eb89e39152fe9f7b6032f
AndersonTM30 <andersondev30@gmail.com>
2022-10-28 21:34:47 -0300

    Criando a classe de criar turma

---
+++ criar-turma.php
@@ -0,0 +1,31 @@
+<?php
+
+use Alura\Pdo\Domain\Model\Student;
+use Alura\Pdo\Infra\Persistence\ConnectionCreator;
+use Alura\Pdo\Infra\Repository\PdoStudentRepository;
+
+require_once 'vendor/autoload.php';
+
+$connection = ConnectionCreator::createConnection();
+
+$studentRepository = new PdoStudentRepository($connection);
+
+$connection->beginTransaction();
+
+$student = new Student(
+    1,
+    'Nico Steppart',
+    new DateTimeImmutable('1985-10-12')
+);
+
+$studentRepository->save($student);
+
+$aStudent = new Student(
+    2,
+    'Doctor Ray',
+    new DateTimeImmutable('1985-11-11')
+);
+
+$studentRepository->save($aStudent);
+
+$connection->commit();
