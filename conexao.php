76f5ec85c9079c454f7f2fb3cf2e4a338bbd9f3a
AndersonTM30 <andersondev30@gmail.com>
2022-10-29 12:01:40 -0300

    Criando a classe de telefone dos alunos

--- conexao.php
+++ conexao.php
@@ -4,4 +4,23 @@ $pdo = new PDO('sqlsrv:Server=localhost,1433;Database=Teste', 'anderson', '1234'
 
 echo "Conectado";
 
-$pdo->exec('CREATE TABLE students (id INT IDENTITY(1,1) PRIMARY KEY, name VARCHAR(200), birth_date VARCHAR(200));');
+$createTableSql = '
+    CREATE TABLE students 
+        (
+            id INT IDENTITY(1,1) PRIMARY KEY,
+            name VARCHAR(200), 
+            birth_date VARCHAR(200)
+        );
+
+    CREATE TABLE phones
+        (
+            id INT IDENTITY(1,1) PRIMARY KEY, 
+            area_code VARCHAR(200), 
+            number VARCHAR(14),
+            student_id INT,
+            FOREIGN KEY (student_id) REFERENCES students(id)
+        );
+
+';
+
+$pdo->exec($createTableSql);
