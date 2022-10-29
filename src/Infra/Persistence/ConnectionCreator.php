8b4848cbda1a06cc181d95b5e2f262ebea0694c2
AndersonTM30 <andersondev30@gmail.com>
2022-10-29 11:24:40 -0300

    Definindo o padrão de consulta no PDO

--- src/Infra/Persistence/ConnectionCreator.php
+++ src/Infra/Persistence/ConnectionCreator.php
@@ -10,6 +10,7 @@ class ConnectionCreator
     {
         $connection =  new PDO('sqlsrv:Server=localhost,1433;Database=Teste', 'anderson', '1234');
         $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);//verifica se tem erro na conexão
+        $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
         return $connection;
         
